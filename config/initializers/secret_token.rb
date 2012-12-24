3c63c5eb51275c5d7a065bfbad370fce486caca7
pjambet <pierre.jambet@gmail.com>
2012-12-24 07:12:21 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LpdcV2::Application.config.secret_token = '1c473202cb6c3abd8a0e6b6e7abd582eae10592e5ce8428c46df848047de96543edf76b98d77f6f4e928d53ab85cb06858afb8ac03a5de7291e10daa5910827c'
