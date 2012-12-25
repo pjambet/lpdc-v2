74b7b4ca88380ffed446d94a201a270e3dba4303
pjambet <pierre.jambet@gmail.com>
2012-12-25 18:07:34 +0100

    Fix display

--- vendor/extensions/products/app/models/refinery/products/product.rb
+++ vendor/extensions/products/app/models/refinery/products/product.rb
@@ -3,13 +3,17 @@ module Refinery
     class Product < Refinery::Core::BaseModel
       self.table_name = 'refinery_products'
 
-      attr_accessible :title, :date, :photo_id, :blurb, :price, :paypal_id, :position, :slug
+      attr_accessible :title, :date, :photo_id, :blurb, :price, :paypal_id, :position, :slug, :weight
 
-      acts_as_indexed :fields => [:title, :blurb]
+      acts_as_indexed fields: [:title, :blurb]
 
-      validates :title, :presence => true, :uniqueness => true
+      validates :title, presence: true, uniqueness: true
 
-      belongs_to :photo, :class_name => '::Refinery::Image'
+      belongs_to :photo, class_name: '::Refinery::Image'
+
+      before_save do
+        self.slug = self.title.parameterize
+      end
     end
   end
 end
